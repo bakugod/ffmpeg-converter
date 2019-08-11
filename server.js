fb3fea7018f1abc38638e75bf1d400a757ad810b
bakugod <bakugod@yandex.ru>
2019-08-11 23:45:41 +0300

    update video-generator

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { spawn, exec } = require('child_process');
 
 const PORT = 8081;
-
 const app = express();
 
 app.use("/", express.static(__dirname + "/"));
@@ -10,7 +9,30 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
-exec('ffmpeg -r 1/5 -i ./input/img%03d.jpg -c:v libx264 -r 30 -pix_fmt yuv420p ./output/test.mp4',() => console.log('done'))
+exec(
+	'ffmpeg -i ./input/img004.png ./input/img004.jpg',
+	(err) => {
+		console.log(err)
+		exec(
+			'ffmpeg -start_number_range 100 -r 1/2 -i ./input/img%03d.jpg -vf scale=720:-1 -r 30 -vframes 500 -pix_fmt yuv420p ./output/test73.mp4 && ffplay ./output/test73.mp4', 
+			(err) => 
+			err
+			? console.error(err.message)
+			: console.log('video processing finished')
+		)
+	}
+)
 
-console.log('app working on', PORT);
-app.listen(PORT)
+// exec(
+// 	'ffmpeg -start_number_range 100 -r 1/2 -i ./input/img%03d.jpg -c:v libx264 -r 30 -vframes 500 -pix_fmt yuv420p ./output/test.mp4', 
+// 	(err) => 
+// 	err
+// 	? console.error(err.message)
+// 	: console.log('video processing finished')
+// )
+
+
+app.listen(
+	PORT, 
+	() => console.log('app working on', PORT)
+);
